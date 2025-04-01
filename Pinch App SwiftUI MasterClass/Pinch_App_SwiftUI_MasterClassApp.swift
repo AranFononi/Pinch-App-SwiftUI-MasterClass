1dadc0c4558f086e88ff3cc12434a8483a39c4c3
Aran Fononi <aaronfononi@gmail.com>
2025-04-02 01:55:33 +0330

    Initial Commit

---
+++ Pinch App SwiftUI MasterClass/Pinch_App_SwiftUI_MasterClassApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinch_App_SwiftUI_MasterClassApp.swift
+//  Pinch App SwiftUI MasterClass
+//
+//  Created by Aran Fononi on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Pinch_App_SwiftUI_MasterClassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
