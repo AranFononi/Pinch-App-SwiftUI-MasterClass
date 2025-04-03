953c3c31c62bd6eb6e4ce3cae1e355b2b078f695
Aran Fononi <aaronfononi@gmail.com>
2025-04-03 16:05:06 +0330

    create long press gesture in new InfoPanelView file

---
+++ Pinch App SwiftUI MasterClass/View/InfoPanelView.swift
@@ -0,0 +1,65 @@
+//
+//  InfoPanelView.swift
+//  Pinch App SwiftUI MasterClass
+//
+//  Created by Aran Fononi on 3/4/25.
+//
+
+import SwiftUI
+
+struct InfoPanelView: View {
+    var scale: CGFloat
+    var offset: CGSize
+    @State private var isInfoPanelVisible: Bool = false
+    
+    var body: some View {
+        HStack {
+         // MARK: - Hotsopt
+            Image(systemName: "circle.circle")
+                .symbolRenderingMode(.hierarchical)
+                .resizable()
+                .frame(width: 30, height: 30)
+                .onLongPressGesture(minimumDuration: 1) {
+                    withAnimation(.easeOut) {
+                        isInfoPanelVisible.toggle()
+                    }
+                }
+            
+            Spacer()
+        // MARK: - Info panel
+            HStack(spacing: 2) {
+                Image(systemName: "arrow.up.left.and.arrow.down.right")
+                Text("\(scale)")
+                
+                Spacer()
+                
+                Image(systemName: "arrow.left.and.right")
+                Text("\(offset.width)")
+                
+                Spacer()
+                
+                Image(systemName: "arrow.up.and.down")
+                Text("\(offset.height)")
+                
+                Spacer()
+                
+                
+            } //: HStack
+            .font(.footnote)
+            .padding(8)
+            .background(.ultraThinMaterial)
+            .clipShape(RoundedRectangle(cornerRadius: 8))
+            .frame(maxWidth: 420)
+            .opacity(isInfoPanelVisible ? 1 : 0)
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    InfoPanelView(scale: 1, offset: .zero)
+        .preferredColorScheme(.dark)
+        .previewLayout(.sizeThatFits)
+        .padding()
+}
