bf14268ce77dc1666e80c2b2ae11b625537aef89
Aran Fononi <aaronfononi@gmail.com>
2025-04-03 11:45:16 +0330

    Add Drag Gesture and a function to reset image state whenever user wants to reset image to original scale and size

--- Pinch App SwiftUI MasterClass/Screen/ContentView.swift
+++ Pinch App SwiftUI MasterClass/Screen/ContentView.swift
@@ -11,8 +11,15 @@ struct ContentView: View {
     // MARK: - Property
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
     
     // MARK: - Function
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     // MARK: - Content
     
@@ -27,6 +34,7 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                     .animation(.linear(duration: 1), value: isAnimating)
                 // MARK: - Tap Gesture
@@ -36,11 +44,28 @@ struct ContentView: View {
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     }
+                // MARK: - Drag Gesture
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                                
+                            }
+                            .onEnded { _ in
+                                withAnimation(.linear(duration: 1)) {
+                                    if imageScale <= 1 {
+                                        resetImageState()
+                                        
+                                    }
+                                    
+                                }
+                            }
+                    )
             } // ZStack
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
