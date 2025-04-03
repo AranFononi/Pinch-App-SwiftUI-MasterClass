c291c525c24f9be7ccd6cb0dd9bbed617fbe375a
Aran Fononi <aaronfononi@gmail.com>
2025-04-03 16:14:58 +0330

    bug fix

--- Pinch App SwiftUI MasterClass/View/InfoPanelView.swift
+++ Pinch App SwiftUI MasterClass/View/InfoPanelView.swift
@@ -66,6 +66,5 @@ struct InfoPanelView: View {
 #Preview {
     InfoPanelView(scale: 1, offset: .zero)
         .preferredColorScheme(.dark)
-        .previewLayout(.sizeThatFits)
         .padding()
 }
