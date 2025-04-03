b47f3acae2d1c3bc6d8bacb0ffc511d73970410f
Aran Fononi <aaronfononi@gmail.com>
2025-04-03 19:37:06 +0330

    Add Magnification Gesture to image

--- Pinch App SwiftUI MasterClass/Screen/ContentView.swift
+++ Pinch App SwiftUI MasterClass/Screen/ContentView.swift
@@ -59,6 +59,26 @@ struct ContentView: View {
                                 }
                             }
                     )
+                // MARK: - Magnification
+                    .gesture(
+                        MagnificationGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    if imageScale >= 1 && imageScale <= 8 {
+                                        imageScale = value
+                                    } else if imageScale > 8 {
+                                        imageScale = 8
+                                    }
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale > 8 {
+                                    imageScale = 8
+                                } else if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
+                    )
             } // ZStack
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
